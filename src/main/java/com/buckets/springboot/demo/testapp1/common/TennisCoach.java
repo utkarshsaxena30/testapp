aa90c1112261c5f0f834c1e3379aaa0329c16958
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2024-02-12 01:11:03 +0530

    Add bean lifecycle hooks

--- src/main/java/com/buckets/springboot/demo/testapp1/common/TennisCoach.java
+++ src/main/java/com/buckets/springboot/demo/testapp1/common/TennisCoach.java
@@ -1,9 +1,21 @@
 package com.buckets.springboot.demo.testapp1.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+
+    @PostConstruct()
+    public void doStuffBeforeInit() {
+        System.out.println("Doing stuff after init");
+    }
+
+    @PreDestroy()
+    public void doStuffBeforeDestroy() {
+        System.out.println("Doing stuff before destroy");
+    }
 
     @Override
     public String getDailyWorkout() {
