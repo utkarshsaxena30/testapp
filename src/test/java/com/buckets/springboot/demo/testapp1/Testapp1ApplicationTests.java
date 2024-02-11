888a0061628baef7793d611408be69cf1674412f
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2024-02-11 21:48:49 +0530

    Initialise repository

---
+++ src/test/java/com/buckets/springboot/demo/testapp1/Testapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buckets.springboot.demo.testapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testapp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
